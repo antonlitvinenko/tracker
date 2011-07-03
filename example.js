257a08b0a1f59ed8c8b2497de00159693a0bc79e
antonlitvinenko <anton.litvinenko@topotun>
2011-07-03 20:19:09 +0300

    introduced vows & express

--- example.js
+++ example.js
@@ -1,8 +1,15 @@
 var http = require('http');
+var urllib = require('url');
 var datastore = [];
 
+
+function okWithJSON(res, data) {
+    res.writeHead(200, {'Content-type':'application/json'});
+    res.end(JSON.stringify(data));
+}
 http.createServer(
     function (req, res) {
+        var url = urllib.parse(req.url);
         if (req.url == '/tasks' && req.method == 'POST') {
             req.addListener("data", function(data) {
                 var p = data.toString().split('=');
@@ -12,16 +19,15 @@ http.createServer(
             });
 
             req.addListener("end", function(data) {
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end();
+                okWithJSON(res, {})
             });
         } else if (req.url == '/tasks' && req.method == 'GET') {
-            res.writeHead(200, {'Content-type':'application/json'});
-            res.end(JSON.stringify(datastore));
-        } else if (req.url == '/tasks' && req.method == 'PUT') {
-            res.writeHead(200, {'Content-type':'application/json'});
-            res.end(JSON.stringify(datastore));
-        }
+            okWithJSON(res, datastore);
+
+        } else if (url.pathname == '/tasks' && req.method == 'PUT') {
+            url.query
+            okWithJSON(res, datastore);
+        }   //sdf
     }).listen(1337, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:1337/');

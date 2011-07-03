24e9cfcc51f0125d26e216a6fc14ff7df58fe0de
antonlitvinenko <anton.litvinenko@topotun>
2011-07-03 19:48:22 +0300

    show and add tasks now working - great success

--- example.js
+++ example.js
@@ -1,18 +1,30 @@
 var http = require('http');
+var datastore = [];
 
 http.createServer(
     function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
 
 
-        if (req.url == '/add' && req.method == 'POST') {
-            req.addListener("data",function(chunk){
-                console.log(chunk);
+        if (req.url == '/tasks' && req.method == 'POST') {
+            req.addListener("data",function(data){
+                var p = data.toString().split('=');
+                if (p[0] == "title") {
+                    //console.log(p[1]);
+                    datastore.push(p[1]);
+                    console.log("ds size: %d; last: %s", datastore.length, datastore[datastore.length-1]);
+                }
+
             });
-            console.log("%o",req.data);
+            //console.log("%o",req.data);
             //var title = require('url').parse(req) // req.getParameter("title");
             //console.log(title);
-            console.log('Yeah baba! sha dobavim task');
+            //console.log('Yeah baba! sha dobavim task');
+        } else if (req.url == '/tasks' && req.method == 'GET') {
+            res.writeHead(200, {'Content-type':'application/json'});
+            res.end(JSON.stringify(datastore));
+
+
         }
 
         res.end('Hello World\n');

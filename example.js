532bf0c2b64f8da9845d761fa60ed9f6f191fb85
antonlitvinenko <anton.litvinenko@topotun>
2011-07-03 19:10:55 +0300

    fignja kotoraja nikomu ne nuzhna

--- example.js
+++ example.js
@@ -4,7 +4,14 @@ http.createServer(
     function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
 
-        if (req.url == '/add') {
+
+        if (req.url == '/add' && req.method == 'POST') {
+            req.addListener("data",function(chunk){
+                console.log(chunk);
+            });
+            console.log("%o",req.data);
+            //var title = require('url').parse(req) // req.getParameter("title");
+            //console.log(title);
             console.log('Yeah baba! sha dobavim task');
         }
 

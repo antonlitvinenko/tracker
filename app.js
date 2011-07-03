40f9df76810400c4cf0c9802ba59a1da71469cac
antonlitvinenko <anton.litvinenko@topotun>
2011-07-03 20:46:08 +0300

    merged code from example.js to app.js (into express)

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -7,32 +6,54 @@ var express = require('express');
 
 var app = module.exports = express.createServer();
 
+var datastore = [];
+
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function() {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function() {
+    app.use(express.errorHandler());
 });
 
 // Routes
 
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Express'
+    });
 });
+
+app.use(express.bodyParser());
+
+app.post('/tasks', function(req, res) {
+    datastore.push(req.body.title);
+    okWithJSON(res, datastore.length-1);
+});
+
+app.get('/tasks', function(req, res) {
+    okWithJSON(res, datastore);
+});
+
+app.put('/tasks/:id', function(req, res) {
+    okWithJSON(res, {});
+});
+
+function okWithJSON(res, data) {
+    res.writeHead(200, {'Content-type':'application/json'});
+    res.end(JSON.stringify(data));
+}
 
 app.listen(1337);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
